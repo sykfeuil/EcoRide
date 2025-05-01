1affdad3737b7fc65ab4f300a550432c82cc5828
sykfeuil <syk.devmail@gmail.com>
2025-05-01 17:46:53 +0200

    Boostrap + packages + home front template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,17 +1,19 @@
 <?php
 
-// src/Controller/MainController.php
+// src/Controller/HomeController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/', name : 'Accueil')]
     public function displayPage(): Response
     {
-        return $this->render('base/header.html.twig');
+        return $this->render('base.html.twig', [
+            'pageToRender' => 'home.html.twig'
+        ]);
     }
 }
